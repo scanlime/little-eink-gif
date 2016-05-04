e2d63e0c10d1b2829963acd605386e1368d04be7
Micah Elizabeth Scott <micah@misc.name>
2016-05-04 12:10:10 -0700

    Fewer whitespace horrors

--- imgprep.py
+++ imgprep.py
@@ -11,64 +11,64 @@ import os
 
 
 def deflate(data, level=9):
-	zData = zlib.compress(data, level)
+    zData = zlib.compress(data, level)
 
-	# Strip off the zlib header, and return the raw DEFLATE data stream.
-	# See the zlib RFC: http://www.gzip.org/zlib/rfc-zlib.html
+    # Strip off the zlib header, and return the raw DEFLATE data stream.
+    # See the zlib RFC: http://www.gzip.org/zlib/rfc-zlib.html
 
-	cmf = ord(zData[0])
-	flg = ord(zData[1])
-	assert (cmf & 0x0F) == 8   # DEFLATE algorithm
-	assert (flg & 0x20) == 0   # No preset dictionary
+    cmf = ord(zData[0])
+    flg = ord(zData[1])
+    assert (cmf & 0x0F) == 8   # DEFLATE algorithm
+    assert (flg & 0x20) == 0   # No preset dictionary
 
-	# Strip off 2-byte header and 4-byte checksum
-	return zData[2:len(zData)-4]
+    # Strip off 2-byte header and 4-byte checksum
+    return zData[2:len(zData)-4]
 
 
 def encode_frame(im):
-	# Convert to 2-bit per pixel grayscale
+    # Convert to 2-bit per pixel grayscale
 
-	bg = Image.new('L', im.size, color=255)
-	rgba = im.convert('RGBA')
-	bg.paste(rgba, mask=rgba.split()[3])
-	gray = bg.tobytes()
+    bg = Image.new('L', im.size, color=255)
+    rgba = im.convert('RGBA')
+    bg.paste(rgba, mask=rgba.split()[3])
+    gray = bg.tobytes()
 
-	fb = ''.join(chr(
-		((ord(gray[i*4 + 0]) >> 6) << 6) |
-		((ord(gray[i*4 + 1]) >> 6) << 4) |
-		((ord(gray[i*4 + 2]) >> 6) << 2) |
-		((ord(gray[i*4 + 3]) >> 6) << 0) )
-		for i in range(len(gray)/4))
+    fb = ''.join(chr(
+        ((ord(gray[i*4 + 0]) >> 6) << 6) |
+        ((ord(gray[i*4 + 1]) >> 6) << 4) |
+        ((ord(gray[i*4 + 2]) >> 6) << 2) |
+        ((ord(gray[i*4 + 3]) >> 6) << 0) )
+        for i in range(len(gray)/4))
 
-	# Compress using the raw Deflate algorithm
-	zfb = deflate(fb)
+    # Compress using the raw Deflate algorithm
+    zfb = deflate(fb)
 
-	# Pad to 4-byte boundary
-	return zfb + chr(0) * (-len(zfb) & 3)
+    # Pad to 4-byte boundary
+    return zfb + chr(0) * (-len(zfb) & 3)
 
 def encode_image(im):
-	assert im.size == (72, 172)
-	results = []
-	while True:
-		millis = im.info.get('duration') or 6000
-		data = encode_frame(im)
-		header = struct.pack("<LL", millis, len(data))
-		results.append(header)
-		results.append(data)
-		try:
-			im.seek(im.tell() + 1)
-		except EOFError:
-			break
-	return ''.join(results)
+    assert im.size == (72, 172)
+    results = []
+    while True:
+        millis = im.info.get('duration') or 6000
+        data = encode_frame(im)
+        header = struct.pack("<LL", millis, len(data))
+        results.append(header)
+        results.append(data)
+        try:
+            im.seek(im.tell() + 1)
+        except EOFError:
+            break
+    return ''.join(results)
 
 
 def main():
-	f = open('images.bin', 'wb')
-	for file in glob('images/*'):
-		if os.path.isfile(file):
-			print('  IMAGE   %s' % file)
-			f.write(encode_image(Image.open(file)))
-	f.close()
+    f = open('images.bin', 'wb')
+    for file in glob('images/*'):
+        if os.path.isfile(file):
+            print('  IMAGE   %s' % file)
+            f.write(encode_image(Image.open(file)))
+    f.close()
 
 if __name__ == '__main__':
-	main()
+    main()
--- main.cpp
+++ main.cpp
@@ -12,32 +12,32 @@ extern const uint32_t _binary_images_bin_start[];
 extern const uint32_t _binary_images_bin_end[];
 
 SPD2701 display( /* SDA  */  GPIOD, GPIO10,
-	             /* SCLK */  GPIOD, GPIO9,
-	             /* CS   */  GPIOD, GPIO8,
-	             /* DC   */  GPIOE, GPIO15,
-	             /* BUSY */  GPIOE, GPIO13 );
+                 /* SCLK */  GPIOD, GPIO9,
+                 /* CS   */  GPIOD, GPIO8,
+                 /* DC   */  GPIOE, GPIO15,
+                 /* BUSY */  GPIOE, GPIO13 );
 
 static void gpio_setup(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOD);
-	rcc_periph_clock_enable(RCC_GPIOE);
-	gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO8 | GPIO9 | GPIO10);
-	gpio_set_mode(GPIOE, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11 | GPIO14 | GPIO15);
-	gpio_set_mode(GPIOE, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO13);
+    rcc_periph_clock_enable(RCC_GPIOD);
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_set_mode(GPIOD, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO8 | GPIO9 | GPIO10);
+    gpio_set_mode(GPIOE, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11 | GPIO14 | GPIO15);
+    gpio_set_mode(GPIOE, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO13);
 
-	// Enter reset
-	gpio_clear(GPIOE, GPIO14);
+    // Enter reset
+    gpio_clear(GPIOE, GPIO14);
 
-	// Tie BS1 low, to indicate 4-wire SPI mode.
-	gpio_clear(GPIOE, GPIO11);
+    // Tie BS1 low, to indicate 4-wire SPI mode.
+    gpio_clear(GPIOE, GPIO11);
 
-	// Leave reset
-	gpio_set(GPIOE, GPIO14);
+    // Leave reset
+    gpio_set(GPIOE, GPIO14);
 }
 
 static void millisec_timer_setup(void)
 {
-	// 72MHz / 8 => 9000000 counts per second
+    // 72MHz / 8 => 9000000 counts per second
     systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
 
     // 9000000/9000 = 1000 overflows per second - every 1ms one interrupt
@@ -50,46 +50,46 @@ static void millisec_timer_setup(void)
 
 void sys_tick_handler(void)
 {
-	millis++;
+    millis++;
 }
 
 static void delay_millisec(unsigned duration)
 {
-	uint32_t deadline = millis + duration;
-	while (int32_t(millis - deadline) < 0);
+    uint32_t deadline = millis + duration;
+    while (int32_t(millis - deadline) < 0);
 }
 
 const uint32_t* show_compressed_frame(const uint32_t *ptr)
 {
-	unsigned duration = ptr[0]; ptr++;
-	unsigned compressed_size = ptr[0]; ptr++;
+    unsigned duration = ptr[0]; ptr++;
+    unsigned compressed_size = ptr[0]; ptr++;
 
-	unsigned long destlen = sizeof display.buffer;
-	unsigned long srclen = compressed_size;
+    unsigned long destlen = sizeof display.buffer;
+    unsigned long srclen = compressed_size;
 
-	display.clear();
-	puff(display.buffer, &destlen, (unsigned char*) ptr, &srclen);
-	ptr += compressed_size >> 2;
+    display.clear();
+    puff(display.buffer, &destlen, (unsigned char*) ptr, &srclen);
+    ptr += compressed_size >> 2;
 
-	display.update();
-	delay_millisec(duration);
+    display.update();
+    delay_millisec(duration);
 
-	return ptr;
+    return ptr;
 }
 
 int main(void)
 {
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
-	gpio_setup();
-	millisec_timer_setup();
-	display.init();
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    gpio_setup();
+    millisec_timer_setup();
+    display.init();
 
-	while (1) {
-		const uint32_t *ptr = _binary_images_bin_start;
-		while (ptr < _binary_images_bin_end) {
-			ptr = show_compressed_frame(ptr);
-		}
-	}
+    while (1) {
+        const uint32_t *ptr = _binary_images_bin_start;
+        while (ptr < _binary_images_bin_end) {
+            ptr = show_compressed_frame(ptr);
+        }
+    }
 
-	return 0;
+    return 0;
 }
